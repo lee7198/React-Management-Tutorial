201f540edc6adb150f8cddf4dbfc282507b59de1
lee7198 <lee7198@naver.com>
2021-05-30 18:30:48 +0900

    Update UI/UX & Add Customer Search Module

--- client/src/components/CustomerDelete.js
+++ client/src/components/CustomerDelete.js
@@ -45,7 +45,7 @@ class CustomerDelete extends React.Component {
           삭제
         </Button>
         <Dialog open={this.state.open} onClose={this.handleClose}>
-          <DialogTitle onClose={this.handleClose}>삭제 경고</DialogTitle>
+          <DialogTitle onClose={this.handleClose}>⚠️ 삭제 경고</DialogTitle>
           <DialogContent>
             <Typography gutterBottom>선택한 고객 정보가 삭제됩니다.</Typography>
           </DialogContent>
